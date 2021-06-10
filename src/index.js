622e398cf22d559532e31358e9852ea211b41674
wangshz <wangshz@yuanian.com>
2021-06-10 12:31:00 +0800

    document.write(),  改写iframe内部的document

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@
 //   document.getElementById('root')
 // );
 
-'use strict';
+// 'use strict';
 
-const e = React.createElement;
+// const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
 
-const domContainer = document.querySelector('#root');
-ReactDOM.render(e(LikeButton), domContainer);
+// const domContainer = document.querySelector('#root');
+// ReactDOM.render(e(LikeButton), domContainer);
