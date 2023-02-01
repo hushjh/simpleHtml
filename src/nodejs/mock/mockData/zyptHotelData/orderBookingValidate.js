862fd5e57c639a727073ebc4c1d0e23927ec9a31
依多宝\77207 <772074834@qq.com>
2023-02-01 10:44:34 +0800

    orderBookingValidate

--- src/nodejs/mock/mockData/zyptHotelData/orderBookingValidate.js
+++ src/nodejs/mock/mockData/zyptHotelData/orderBookingValidate.js
@@ -1,22 +1,26 @@
 const action = () => {
   return {
-    "code": 10000,
-    "data": {
-      "type": 0,
-      "message": "验证通过、可预订",
-      "data": {
-        "avgprice": 1,
-        "nightlyrates": [
+    code: 10000,
+    data: {
+      type: 1,
+      message: "抱歉，房型价格已发生变化，当前价格为23元",
+      data: {
+        avgprice: 23,
+        nightlyrates: [
           {
-            "price": null,
-            "otherAmout": null,
-            "date": null,
-            "identification": null
-          }
-        ]
-      }
+            price: 23,
+            otherAmout: 0,
+            status: true,
+            date: "2023-02-05",
+            mealType: 3,
+            mealNum: 0,
+            identification:
+              "a1d44436da958ad76498bc6bfbb29efe6e9071a0e482218f81d05662e750aee1bb74dbc23e1a8ad42b658144a6387fba",
+          },
+        ],
+      },
     },
-    "message": ""
-  }
+    message: "",
+  };
 }
 module.exports = action();
